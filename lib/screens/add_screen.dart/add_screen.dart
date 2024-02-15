5925ba92c57317e52075e322aa98e1dcee241888
Sharifjon <sharifboymuminov@gmail.com>
2024-02-15 13:23:25 +0500

    asdf

--- lib/screens/add_screen.dart/add_screen.dart
+++ lib/screens/add_screen.dart/add_screen.dart
@@ -4,6 +4,7 @@ import 'package:default_project/utils/app_images.dart';
 import 'package:default_project/utils/size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 class AddScreen extends StatefulWidget {
@@ -39,11 +40,28 @@ class _AddScreenState extends State<AddScreen> {
                     icon: AppImages.arrowBack,
                     onTab: () {},
                   ),
-                  Spacer(),
+                  const Spacer(),
                   ButtonTop(icon: AppImages.look, onTab: () {}),
                   21.getW(),
                   ButtonTop(icon: AppImages.save, onTab: () {}),
                 ],
+              ),
+              TextField(
+                cursorColor: AppColors.c_CCCCCC,
+                style: TextStyle(
+                  color: AppColors.c_FFFFFF,
+                  fontSize: 35.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+                maxLines: 6,
+                decoration: InputDecoration(
+                  hintText: "Title",
+                  hintStyle: TextStyle(
+                    color: AppColors.c_9A9A9A,
+                    fontSize: 48.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
               ),
             ],
           ),
