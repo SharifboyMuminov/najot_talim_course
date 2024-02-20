c4a1d324ca75246dbbc936be444ae3dd61c19207
Sharifjon <sharifboymuminov@gmail.com>
2024-02-20 20:28:57 +0500

    Hello

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+    );
   }
 }
