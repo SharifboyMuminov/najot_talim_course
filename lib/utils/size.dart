aa91dfcd0719414e1bd48040c8d1773a07edbba0
Sharifjon <sharifboymuminov@gmail.com>
2024-02-22 21:37:09 +0500

    Set default File

--- lib/utils/size.dart
+++ lib/utils/size.dart
@@ -7,6 +7,6 @@ extension Size on int {
   double get he => (this / 812) * height;
   double get we => (this / 375) * width;
 
-  SizedBox getH() => SizedBox(height: this.he);
-  SizedBox getW() => SizedBox(height: this.we);
+  SizedBox getH() => SizedBox(height: (this / 812) * height);
+  SizedBox getW() => SizedBox(height: (this / 375) * width);
 }
